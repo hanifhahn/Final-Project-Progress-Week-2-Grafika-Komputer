8aefa5900ed62ad001fa871c17d720cf1b3abeba
hanifhahn <84712704+hanifhahn@users.noreply.github.com>
2022-11-04 16:43:11 +0700

    Add files via upload

---
+++ Lesson04/Demo.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2, VBO3, VAO3, EBO3, texture3;
+	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
+
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow *window);
+
+	void BuildColoredCubeSand();
+	void BuildColoredCubeWater();
+
+	void DrawColoredCubeSand(double xPos, double yPos, double zPos);
+	void DrawColoredCubeWater(double xPos, double yPos, double zPos);
+
+	void BuildColoredPlane();
+	void DrawColoredPlane();
+
+	void MoveCamera(float speed);
+	void StrafeCamera(float speed);
+	void YCamera(float speed);
+	void RotateCamera(float speed);
+	void InitCamera();
+};
+
